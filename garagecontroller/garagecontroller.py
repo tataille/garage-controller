d7ee78c37193f8ecea8449bdf16630817c7a6df5
tataille <jean.marc.taillant@gmail.com>
2023-05-10 19:17:29 +0200

    Update casing

--- garagecontroller/garagecontroller.py
+++ garagecontroller/garagecontroller.py
@@ -28,14 +28,17 @@ client.on_message = callback.on_message;
 client.on_publish = callback.on_publish;
 client.on_subscribe = callback.on_subscribe;
 
-myport = int(os.getenv('port'))
-mybroker = os.getenv('broker')
-print(mybroker)
+broker_port = int(os.getenv('port'))
+broker_host = os.getenv('broker')
+door_sensor_topic = os.getenv('doorsensortopic')
+
+print('MQTT Broker: '+broker_host+':'+broker_port)
+print('Door sensor topic: '+door_sensor_topic)
 
 client.will_set('home/garagedoor/availability','offline',retain=False)
 
-client.connect(mybroker,
-                myport,
+client.connect(broker_host,
+                broker_port,
                 keepalive=60,
                 bind_address="")
 atexit.register(disconnectMQTT)
