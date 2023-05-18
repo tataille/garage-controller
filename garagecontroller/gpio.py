ca60562b099883755c3b61726900ef72ce22a56a
tataille <jean.marc.taillant@gmail.com>
2023-05-18 16:44:44 +0200

    enhance button

--- garagecontroller/gpio.py
+++ garagecontroller/gpio.py
@@ -1,10 +1,12 @@
 import RPi.GPIO as gpio
+import threading
 import time
 from signal import signal, SIGINT
 from sys import exit
 
 LED = 12 #Définit le numéro du port GPIO qui alimente la led
-button=36
+button=16
+time=100
 
 def handler(signal_received, frame):
     # on gère un cleanup propre
@@ -24,6 +26,12 @@ def push():
     gpio.output(14, gpio.HIGH)
     gpio.output(LED, gpio.LOW) #On l'éteind
 
+def check_button():
+    while 1:
+        if (gpio.input(button) == 1):
+            push()
+        time.sleep(time/1000)
+
 
 def init():
     # on passe en mode BMC qui veut dire que nous allons utiliser directement
@@ -34,8 +42,5 @@ def init():
     gpio.setup(14, gpio.OUT, initial=1)
     gpio.setup(LED, gpio.OUT, initial=0) #Active le contrôle du GPIO
     gpio.setup(button, gpio.IN)
-
+    gpio.add_event_detect(button, gpio.RISING, callback=push, bouncetime=200)
     signal(SIGINT, handler)
-    while 1:
-        if (gpio.input(button) == 1):
-            push()
