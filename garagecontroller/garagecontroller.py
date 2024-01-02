742cebf14786c57f55dae9a70ecb3676f65c384b
Jean Marc TAILLANT <jean.marc.taillant@gmail.com>
2024-01-02 21:10:52 +0000

    fix memory leak

--- garagecontroller/garagecontroller.py
+++ garagecontroller/garagecontroller.py
@@ -55,6 +55,8 @@ def on_message(client, userdata, message,tmp=None):
                client.publish(state_topic,payload='closed')
           else:
                client.publish(state_topic,payload='opened')
+          m_in = None
+          m_decode = None
     
 def on_publish(client, userdata, mid,tmp=None):
     print(dt.now().strftime("%H:%M:%S.%f")[:-2] + " Published message id: "+str(mid))
